0b9637e3dd6958a46ed4848acf9e250b0ae374a9
alef <45402388+elviale@users.noreply.github.com>
2025-11-04 18:33:37 -0300

    iniciodecarrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,1 +1,30 @@
 console.log("Enlazado a js-Carrusel")
+// Array con imágenes de ciclismo 
+const imagenes = [ "ImagenesCycle/Carrusel2.jpeg", "ImagenesCycle/Carrusel3.jpeg", "ImagenesCycle/Carrusel4.jpeg"
+  
+];
+
+// Índice actual
+let indice = 0;
+
+// Obtener el elemento de la imagen
+const img = document.getElementById("imagen");
+img.src = imagenes[indice];
+
+// Función para avanzar
+function avanzar() {
+  indice++;
+  if (indice >= imagenes.length) {
+    indice = 0; // vuelve al inicio
+  }
+  img.src = imagenes[indice];
+}
+
+// Función para retroceder
+function retroceder() {
+  indice--;
+  if (indice < 0) {
+    indice = imagenes.length - 1; // vuelve al final
+  }
+  img.src = imagenes[indice];
+}
